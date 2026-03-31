8ced2f647d25af4510ade6b720ecda4ef093b3ec
AttilaB <attilabloom7299@gmail.com>
2026-03-31 15:58:57 -0400

    added matrix population

--- matrix/Matrix.java
+++ matrix/Matrix.java
@@ -1,2 +1,52 @@
+import java.util.Random;
+
 public class Matrix {
+  int size;
+  int minInput = 1;
+  int maxInput = size * size;
+  Random random = new Random();
+
+  public Matrix(int size){
+    printDefault(size);
+    populateMatrix(size);
+  }
+
+  public void populateMatrix(int size){
+    this.size = size;
+    int[][] matrix = new int[size][size];
+    System.out.println();
+    System.out.print("Populating matrix...");
+    for (int y = 0; y < size; y++) {
+      for (int x = 0; x < size; x++) {
+        int randNum = random.nextInt(1, (size^2));
+        matrix[x][y] = randNum;
+      }
+    }
+    System.out.println("Matrix populated");
+    System.out.println();
+    printMatrix(matrix);
+  }
+
+  public void printDefault(int size){
+    int[][] matrix = new int[size][size];
+    System.out.println("Your matrix is " + size + " x " + size);
+    System.out.println();
+    System.out.println("Printing matrix with default values");
+    System.out.println();
+    for (int y = 0; y < size; y++) {
+      for (int x = 0; x < size; x++) {
+        System.out.print(matrix[x][y] + "   ");
+      }
+      System.out.println();
+    }
+  }
+
+  public void printMatrix(int[][] matrix) {
+    for (int y = 0; y < size; y++) {
+      for (int x = 0; x < size; x++) {
+        System.out.print(matrix[x][y] + "   ");
+      }
+      System.out.println();
+    }
+  }
 }
--- matrix/RunMatrix.java
+++ matrix/RunMatrix.java
@@ -2,5 +2,10 @@ import java.util.Scanner;
 
 public class RunMatrix {
   public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Please enter the size of your matrix: ");
+    int input = scanner.nextInt();
+    Matrix matrix = new Matrix(input);
+    scanner.close();
   }
 }
